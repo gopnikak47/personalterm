ac4b194355b8a3b5942d9f757638150d081afe15
Onimeleon <65282158+gopnikak47@users.noreply.github.com>
2023-08-09 15:27:29 +0700

    Update api_commands.ts
    translated to Vietnamese

--- src/utils/bin/api_commands.ts
+++ src/utils/bin/api_commands.ts
@@ -22,14 +22,14 @@ export const quote = async (args: string[]): Promise<string> => {
 
 export const readme = async (args: string[]): Promise<string> => {
   const readme = await getReadme();
-  return `Opening GitHub README...\n
+  return `Đang mở README GitHub của tớ...\n
   ${readme}`;
 };
 
 export const weather = async (args: string[]): Promise<string> => {
   const city = args.join('+');
   if (!city) {
-    return 'Usage: weather [city]. Example: weather casablanca';
+    return 'Cách sử dụng: weather [địa điểm]. Ví dụ: weather hanoi';
   }
   const weather = await getWeather(city);
   return weather;
